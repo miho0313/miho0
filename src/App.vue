ee88933dfd0f1db9a36be540ab4a78036587e270
miho <sshm03132@gmail.com>
2025-04-15 21:51:44 +0900

    To-do-List.html

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <img alt="Vue logo" src="./assets/nnn.jpg">
+
+  <div class="menu">
+    <a v-for="a in 메뉴들" :key="a">{{ a }} </a>
+
+  </div>
+
+  <div>
+    <h4>{{ products[0] }}</h4>
+    <p>과제하기</p>
+    <p>코딩공부하기</p>
+    <p>코딩하기</p>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>밥먹기</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p></p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      products : ['일1','일2','일3'],
+    }
+  },
+  components: {
+   
+  }
+
+</script>
+
+<style>
+body{
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
